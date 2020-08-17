0d272b67c8352e75bbdaf32363ff566e2855e9a4
Luiz Eduardo <servergta@gmail.com>
2020-08-17 12:10:28 -0300

    Add messages during WebOS add script

--- webos/add_webos.sh
+++ webos/add_webos.sh
@@ -11,13 +11,18 @@ then
 fi
 
 # Tries to remove the device first
+echo "=== Device setup for $WEBOS_DEVICE"
+echo "= Trying to remove $WEBOS_DEVICE"
 ares-setup-device -r $WEBOS_DEVICE
 
 # Adds the device using defined name and ip
+echo "= Re-adding $WEBOS_DEVICE"
 ares-setup-device -a $WEBOS_DEVICE -i "{'username':'prisoner', 'host':'$WEBOS_DEVICE_IP','port':'9922'}"
 
 # Gets the authorization key, will prompt for the device passphrase
+echo "= Getting key on $WEBOS_DEVICE"
 ares-novacom -d $WEBOS_DEVICE --getkey
 
 # Shows dev apps installed on device to check key authorization
+echo "= Apps installed on $WEBOS_DEVICE"
 ares-install -d $WEBOS_DEVICE --list
